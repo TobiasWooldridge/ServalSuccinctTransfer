f7c5f3e54b74cef5c8880afda9faf494ab7f5d01
Tobias Wooldridge <tobias@wooldridge.id.au>
2014-05-27 17:12:46 +0930

    Retransmission implemented
    There's one more step necessary for retransmission, which is adding some system to chase up packets which have been delayed for a long period of time or are otherwise holding up existing packets.
    
    This needs to be done such that simple out-of-order transmissions do not cause retransmission requests to be sent

--- src/org/servalproject/succinct/simulator/Main.java
+++ src/org/servalproject/succinct/simulator/Main.java
@@ -7,16 +7,32 @@ public class Main {
     public static class PacketEndPoint {
         private PacketEndPoint endPoint;
 
+        private String name;
+
         int nextSeqNumber = 0;
 
+        Map<Integer, Packet> sentPackets = new HashMap<>();
         Map<Integer, Packet> receivedPackets = new HashMap<>();
+
+        public PacketEndPoint(String name) {
+            this.name = name;
+        }
+
 
         public void connectTo(PacketEndPoint pep) {
             endPoint = pep;
-            pep.endPoint = endPoint;
+            pep.endPoint = this;
         }
 
         public void sendPacket(Packet packet) {
+            sentPackets.put(packet.getSequenceNumber(), packet);
+            endPoint.receivePacket(packet);
+        }
+
+
+        private void requestRetransmission(int sequenceNumber) {
+            System.out.println(name + ": " + "Requesting retransmission of " + sequenceNumber);
+            Packet packet = new Packet(Packet.PacketType.REQ, sequenceNumber, new byte[] {});
             endPoint.receivePacket(packet);
         }
 
@@ -24,15 +40,31 @@ public class Main {
             // Verify the hash of the packet to make sure it arrived correctly
             // -> if hash is invalid, drop packet
             if (!packet.verifyChecksum()) {
-                System.err.println("Dropped illegal packet " + packet.getSequenceNumber());
+                System.out.println(name + ": " + "Dropped packet due to invalid hash data");
+
+                // If the packet isn't absurdly far in the future, or in the past, request it is retransmitted
+                if (packet.getSequenceNumber() - nextSeqNumber < 10 && !receivedPackets.containsKey(packet.getSequenceNumber())) {
+                    requestRetransmission(packet.getSequenceNumber());
+                }
+
                 return;
             }
 
-            if (packet.getType() == Packet.PacketType.DATA) {
+
+            if (packet.getType() == Packet.PacketType.REQ) {
+                if (sentPackets.containsKey(packet.getSequenceNumber())) {
+                    sendPacket(sentPackets.get(packet.getSequenceNumber()));
+                }
+                else {
+                    System.out.println(name + ": " + "Other endpoint requested I retransmit packet #" + packet.getSequenceNumber() + ", but I don't have it!");
+                }
+
+            }
+            else if (packet.getType() == Packet.PacketType.DATA) {
                 // If it has already been received, do nothing
                 int newSeqNumber = packet.getSequenceNumber();
                 if (receivedPackets.containsKey(newSeqNumber) || newSeqNumber < nextSeqNumber) {
-                    System.err.println("Received packet #" + newSeqNumber + " a second time");
+                    System.out.println(name + ": " + "Received packet #" + newSeqNumber + " a second time");
                     return;
                 }
 
@@ -41,7 +73,7 @@ public class Main {
 
                 // Don't process further if it's not the next packet
                 if (newSeqNumber > nextSeqNumber) {
-                    System.out.println("Received packet #" + newSeqNumber + " early, waiting for packet " + nextSeqNumber);
+                    System.out.println(name + ": " + "Received packet #" + newSeqNumber + " early, waiting for packet " + nextSeqNumber);
                     return;
                 }
 
@@ -49,7 +81,7 @@ public class Main {
                 while (receivedPackets.containsKey(nextSeqNumber)) {
                     Packet p = receivedPackets.remove(nextSeqNumber);
 
-                    System.out.println("Done with packet " + p.getSequenceNumber());
+                    System.out.println(name + ": " + "Done with packet " + p.getSequenceNumber());
 
                     nextSeqNumber++;
                 }
@@ -64,15 +96,17 @@ public class Main {
 
         Packet[] packets = packetMagic.packetize(message, 10, 0);
 
-        PacketEndPoint from = new PacketEndPoint();
-        PacketEndPoint to = new PacketEndPoint();
+        PacketEndPoint from = new PacketEndPoint("Origin");
+        PacketEndPoint to = new PacketEndPoint("Destination");
 
         from.connectTo(to);
 
         Random rand = new Random();
 
         for (int i = 0; i < 20; i++) {
-            from.sendPacket(packets[rand.nextInt(packets.length)]);
+            Packet packet = packets[rand.nextInt(packets.length)];
+            System.out.println("NOW SENDING PACKET " + packet.getSequenceNumber());
+            from.sendPacket(packet);
         }
     }
 }
